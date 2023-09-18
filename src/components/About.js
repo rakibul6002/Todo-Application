271d2d646887bc5a706a37a2ed65ddb04535bfd8
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-18 22:37:54 +0600

    abcd

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,7 @@ const About = () => {
       <div className=' flex items-center justify-center'>
         <p className='mt-10 text-4xl border-b-2 text-gray-300 font-semibold'>About Us</p>
       </div>
-      <div className=' flex flex-col items-center justify-center mt-32'>
-        <p className=' text-3xl  text-gray-300'>WELCOME to our Todo Application</p>
-        <div className=' flex flex-col items-center justify-center mt-4'>
-
-          <p className=' text-xl  text-gray-300'>This is a todo application.</p>
-          <p className=' text-xl  text-gray-300'>It's help you to store your data.</p>
-        </div>
-      </div>
+      <p>jhjhjhhjk</p>
     </div>
   )
 }
