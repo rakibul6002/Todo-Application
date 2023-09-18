ca9e0d877c589b900cc70e188cad2f8a2300a0c1
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-18 22:19:19 +0600

    a

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='w-full h-screen bg-bg-img bg-cover flex flex-col items-center justify-center'>
+      <p>About</p>
+    </div>
   )
 }
 
