4fbc1f60602728692fbce751aa619298d827c636
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-18 23:01:48 +0600

    hlp

--- src/app/Help/page.js
+++ src/app/Help/page.js
@@ -1,12 +1,10 @@
+import Hlp from '@/components/Hlp'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        <p className='flex items-center justify-center mt-10'>
-
-        Help Page Created...
-        </p>
+        <Hlp/>
     </div>
   )
 }
---
+++ src/components/Hlp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hlp = () => {
+  return (
+    <div className='w-full h-screen bg-bg-img bg-cover'>
+         <div className=' flex flex-col items-center justify-center'>
+        <p className='mt-10 text-4xl border-b-2 text-gray-300 font-semibold'>User Guide</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hlp
