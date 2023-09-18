92ccd89675eee86715b9f673bfa61f1bfc496694
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-18 22:41:12 +0600

    wel

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-full h-screen bg-bg-img bg-cover'>
+    <main className='w-full h-screen bg-bg-img bg-cover'>
       <div className=' flex items-center justify-center'>
         <p className='mt-10 text-4xl border-b-2 text-gray-300 font-semibold'>About Us</p>
       </div>
-      <p>jhjhjhhjk</p>
-    </div>
+      <div className=' flex items-center justify-center'>
+      <p className='mt-32 text-4xl font-semibold'>WELCOME to our 'TODO APPLICATION'</p>
+      </div>
+    </main>
   )
 }
 
