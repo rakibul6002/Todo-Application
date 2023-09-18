1d92d92484313c0e1f8f9219bea075783463af8c
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-18 23:08:21 +0600

    hplkp

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,7 @@ const About = () => {
         <p className='mt-10 text-4xl border-b-2 text-gray-300 font-semibold'>About Us</p>
       </div>
      <div>
-        <div className='flex flex-col items-center justify-center mt-8 text-2xl  text-white '>
-
-          <p>This is a todo application.</p>
-          <p>It's help you to store your data.</p>
-
-        </div>
+       <p>kuddus</p>
      </div>
     </main>
   )
