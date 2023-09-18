48a1312a068b0705c6d47cacbf8681bc26f79c96
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-18 22:55:48 +0600

    asd

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,6 @@ const About = () => {
     <main className='w-full h-screen bg-bg-img bg-cover'>
       <div className=' flex flex-col items-center justify-center'>
         <p className='mt-10 text-4xl border-b-2 text-gray-300 font-semibold'>About Us</p>
-        <p>WELCOME to our 'TODO APPLICATION'</p>
       </div>
       
     </main>
