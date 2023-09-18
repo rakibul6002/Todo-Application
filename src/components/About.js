233d66d476262eaeec2e2608a2ec41cf118c7f5c
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-18 22:33:37 +0600

    ab

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-full h-screen bg-bg-img bg-cover flex flex-col items-center justify-center'>
-      <p>About</p>
+    <div className='w-full h-screen bg-bg-img bg-cover'>
+      <div className=' flex items-center justify-center'>
+        <p className='mt-10 text-4xl border-b-2 text-gray-300 font-semibold'>About Us</p>
+      </div>
+      <div className=' flex flex-col items-center justify-center mt-32'>
+        <p className=' text-3xl  text-gray-300'>WELCOME to our Todo Application</p>
+        <div className=' flex flex-col items-center justify-center mt-4'>
+
+          <p className=' text-xl  text-gray-300'>This is a todo application.</p>
+          <p className=' text-xl  text-gray-300'>It's help you to store your data.</p>
+        </div>
+      </div>
     </div>
   )
 }
