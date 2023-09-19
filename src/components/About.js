32c27e688d4fde40bf9424e1e41d89b3d8ddd160
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-19 15:14:54 +0600

    abRf

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
         <p className='mt-10 text-4xl border-b-2 text-gray-300 font-semibold'>About Us</p>
       </div>
      <div className='flex flex-col items-center justify-center mt-32'>
-       <p>kuddus</p>
+       <p>Welcome to our TODO APPLICATION...</p>
      </div>
     </main>
   )
