ad2c845d63179a2098106ec17e803c17c49cd387
rakibul6002 <mdrakibulislam22040@gmail.com>
2023-09-19 15:18:26 +0600

    abyf

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ const About = () => {
       <div className=' flex flex-col items-center justify-center'>
         <p className='mt-10 text-4xl border-b-2 text-gray-300 font-semibold'>About Us</p>
       </div>
-     <div className='flex flex-col items-center justify-center mt-32'>
-       <p>Welcome to our TODO APPLICATION...</p>
+     <div className='flex flex-col items-center justify-center mt-32  text-gray-300'>
+       <p className='text-3xl'>Welcome to our TODO APPLICATION...</p>
      </div>
     </main>
   )
